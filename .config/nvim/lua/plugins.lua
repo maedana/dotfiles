c5de1b6c635e4a65db80ab97f244d32a4416bf5d
maedana <maeda.na@gmail.com>
2024-02-25 09:57:36 +0900

    コミット漏れかな

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -69,9 +69,8 @@ require("packer").startup(function()
     setup = function()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fg', builtin.grep_string, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>of', builtin.oldfiles, {})
     end,
   }
 
