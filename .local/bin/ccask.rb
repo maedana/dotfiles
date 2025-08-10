372ebb426af591ab45537006a5450998e22a2138
maedana <maeda.na@gmail.com>
2025-08-10 09:18:43 +0900

    ccaskで対象プロジェクトわかるように改善

--- .local/bin/ccask.rb
+++ .local/bin/ccask.rb
@@ -3,8 +3,21 @@
 
 require 'json'
 
-title = "[CC] 要確認"
-content = "詳細"
+# 標準入力からJSONを読み取ってパース
+stdin_content = STDIN.read
+begin
+  data = JSON.parse(stdin_content)
+  if data['cwd']
+    project_name = File.basename(data['cwd'])
+    title = "[CC] #{project_name} 要確認"
+  else
+    title = "[CC] 要確認"
+  end
+  content = data['message'] || "詳細"
+rescue JSON::ParserError
+  title = "[CC] 要確認"
+  content = "詳細"
+end
 
 curl_command = [
   'curl', '-X', 'POST',
