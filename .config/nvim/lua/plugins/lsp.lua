bd527888e34cae139f8fee3a1f85b1c48d7e415a
maedana <maeda.na@gmail.com>
2026-05-06 11:22:15 +0900

    step 4: Neovim 0.12内蔵補完を有効化
    - nvim-cmpは使わず vim.lsp.completion を使用
    - 全キー入力でautotrigger発動するよう triggerCharacters を拡張
    - completeoptを menuone,noselect,popup に変更

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -31,6 +31,19 @@ return {
           },
         },
       })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("my.lsp", {}),
+        callback = function(ev)
+          local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+          if client:supports_method("textDocument/completion") then
+            local chars = {}
+            for i = 32, 126 do table.insert(chars, string.char(i)) end
+            client.server_capabilities.completionProvider.triggerCharacters = chars
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+          end
+        end,
+      })
     end,
   },
 }
