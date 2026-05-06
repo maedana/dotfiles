0d6c5bb29fd2eaf3dc9de02ef9f1049c3685c701
maedana <maeda.na@gmail.com>
2026-05-06 11:47:02 +0900

    JS/TS/Vueのフォーマッタをbiomeのみに変更

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -15,9 +15,9 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
-        javascript = { "biome", "eslint_d", stop_after_first = true },
-        typescript = { "biome", "eslint_d", stop_after_first = true },
-        vue = { "biome", "eslint_d", stop_after_first = true },
+        javascript = { "biome" },
+        typescript = { "biome" },
+        vue = { "biome" },
         ruby = { "rubocop" },
       },
       formatters = {
